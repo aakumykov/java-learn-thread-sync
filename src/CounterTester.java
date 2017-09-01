891115d834d4b78b63a08ab0c081a98f26b681ef
Андрей Кумыков <aakumykov@yandex.ru>
2017-09-01 09:51:33 +0700

    1

---
+++ src/CounterTester.java
@@ -0,0 +1,49 @@
+package my.tutorial.counter;
+ 
+public class CounterTester 
+{
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("CounterTester.main()");
+
+        Counter counter = new Counter();
+
+        for(int i=0; i<10; i++) {
+            CounterThread ct = new CounterThread(counter);
+            ct.start();
+            // ct.join(1000);
+        }
+        Thread.sleep(1000);
+        
+        System.out.println("Counter:" + counter.getCounter());
+    }
+}
+ 
+class Counter
+{
+    private long counter = 0L;
+    
+    public void increaseCounter() {
+        counter++;
+    }
+    
+    public long getCounter() {
+        return counter;
+    }
+}
+ 
+class CounterThread extends Thread
+{
+    private Counter counter;
+    
+    public CounterThread(Counter counter) {
+        System.out.println("Создаётся CounterThread");
+        this.counter = counter;
+    }
+    
+    @Override
+    public void run() {
+        for(int i=0; i<1000; i++) {
+            counter.increaseCounter();
+        }
+    }
+}
