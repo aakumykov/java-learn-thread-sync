b403f64c0cfd0e62346ac5f524ccf15b0ea885ee
Андрей Кумыков <aakumykov@yandex.ru>
2017-09-02 18:56:40 +0700

    Пока с wait() не получается.

--- src/CounterTester.java
+++ src/CounterTester.java
@@ -1,72 +1,68 @@
 package my.tutorial.counter;
  
-public class CounterTester 
+public class CounterTester
 {
+    private static int counter = 0;
+    private static int aliveCount = 0;
+    
+    public static int getCounter() { return counter; }
+    public synchronized void increaseCounter() { counter++; }
+
+
     public static void main(String[] args) throws InterruptedException {
         
-        int max = 50;
+        CounterTester tester = new CounterTester();
         
-        Counter counter = new Counter();
-        
-        CounterThread[] pool = new CounterThread[max];
+        int max = 2;
+        aliveCount = max;
 
+        CounterThread[] threadPool = new CounterThread[max];
+
+        // создаю группу потоков
         for(int i=0; i<max; i++) {
-            CounterThread ct = new CounterThread(counter, i);
-            pool[i] = ct;
-            pool[i].start();
-            // ct.join(1000); // коллизий нет, котому что ждём, пока поток завершится (отработает)
+            CounterThread ct = new CounterThread(tester, i);
+            threadPool[i] = ct;
+            threadPool[i].start();
         }
         
-
-        //boolean alive = false;
-        int aliveCount = 0;
+        // жду завершения и считаю
+        while (0 != aliveCount) {
+            try { 
+                tester.wait();
+            } catch (InterruptedException e) {
+                aliveCount--;
+            }
+        }
         
-        for (CounterThread item: pool) {
-			//System.out.println("item in pool");
-			if (item.isAlive()) { aliveCount++; }
-		}
-		
-		Thread.sleep(1000);
-        
-        System.out.println("aliveCount:" + aliveCount);
-        System.out.println("Counter:" + counter.getCounter());
-    }
-}
- 
-class Counter
-{
-    private long counter = 0L;
-    
-    //public void increaseCounter() {
-    public synchronized void increaseCounter() {
-        counter++;
-    }
-    
-    public long getCounter() {
-        return counter;
+        // вывожу результат
+        System.out.println("Счётчик=" + getCounter());
     }
 }
  
 class CounterThread extends Thread
 {
-    private Counter counter;
+    private CounterTester tester;
     private int index = 0;
     
-    public CounterThread(Counter counter, int index) {
-        // System.out.println("Создаётся CounterThread");
-        this.counter = counter;
+    public CounterThread(CounterTester tester, int index) {
+        this.tester = tester;
         this.index = index;
+        // System.out.println("Создаётся CounterThread-"+this.index);
     }
     
     @Override
     public void run() {
-        for(int i=0; i<1000; i++) { counter.increaseCounter(); }
+        for(int i=0; i<1000; i++) { 
+            tester.increaseCounter();
+        }
+
         try { 
 			Thread.sleep(10); 
 		} catch (InterruptedException e) {
 			System.out.println("CounterThread-"+index+": поймано исключение InterruptedException");
 		} finally {
-            System.out.println("CounterThread-"+index+": обязательный блок в CounterThread");
+            // System.out.println("CounterThread-"+index+": обязательный блок в CounterThread");
+            tester.notify();
         }
     }
 }
